60d1b7618bc9e2a7e1067c708117e8cda8e03927
COLE CARROLL <YOUR EMAIL ADDRESS>
2017-10-23 20:46:04 -0600

    test

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-require('dotenv').config() 
+require('dotenv').config()
 
 const app = express()
-const port = process.env.PORT || 3000 
+const port = process.env.PORT || 3000
 
 app.set('view engine', 'hbs');
 app.use(express.static('public'));
@@ -11,11 +11,10 @@ app.get('/', (req, res) => {
     res.render('index');
 })
 
-app.get('/login', (req, res)=>{
+app.get('/login', (req, res) => {
     res.render('login')
 })
 
 app.listen(port, (req, res) => {
     console.log('working')
 })
-
